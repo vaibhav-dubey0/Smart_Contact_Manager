46c6149e87edc65d9a07471fb845afac9acbf195
Vaibhav Dubey <dubeyvaibhav482@gmail.com>
2024-11-09 00:28:40 +0530

    Fourth

--- src/main/java/com/smcomanager/smartcontactmanager/Controllers/requestController.java
+++ src/main/java/com/smcomanager/smartcontactmanager/Controllers/requestController.java
@@ -1,17 +1,20 @@
 package com.smcomanager.smartcontactmanager.Controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 
 @Controller
 public class requestController {
 
-@GetMapping("/")
+    @GetMapping("/")
 public String getMethodName() {
-    return "index";
+    return "home";
 }
+
 
 @GetMapping("/about")
 public String getAbouut(){
