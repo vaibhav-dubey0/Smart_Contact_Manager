feb36f74ca2e75f06f953bc6862de9df9275df6a
Vaibhav Dubey <dubeyvaibhav482@gmail.com>
2024-11-14 00:37:24 +0530

    Ten

--- src/main/java/com/smcomanager/smartcontactmanager/Controllers/requestController.java
+++ src/main/java/com/smcomanager/smartcontactmanager/Controllers/requestController.java
@@ -2,17 +2,23 @@ package com.smcomanager.smartcontactmanager.Controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-
+import com.smcomanager.smartcontactmanager.Form_Handler.UserForm;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class requestController {
 
 @GetMapping("/register")
-    public String getSignup(){
-    return "register";
+    public String getSignup(Model model){
+        UserForm userForm=new UserForm();
+        model.addAttribute("userForm", userForm);
+      
+        return "register";
 }
 
  @GetMapping("/")
@@ -36,5 +42,12 @@ public String getContact(){
     return "contact";
 }
 
+   @PostMapping("/do-register")
+   public String postMethodName(@ModelAttribute UserForm userForm) {
+        System.out.print(userForm.getName());
+        System.out.print(userForm);
+       return "redirect:register";
+   }
+   
 
 }
