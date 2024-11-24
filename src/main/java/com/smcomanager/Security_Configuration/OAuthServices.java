28ff04b77cd709a567caf191c4e40c39d1499df0
Vaibhav Dubey <dubeyvaibhav482@gmail.com>
2024-11-24 22:27:40 +0530

    t

--- src/main/java/com/smcomanager/Security_Configuration/OAuthServices.java
+++ src/main/java/com/smcomanager/Security_Configuration/OAuthServices.java
@@ -3,7 +3,6 @@ package com.smcomanager.Security_Configuration;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import com.smcomanager.Helper.AppConstent;
 import com.smcomanager.Repository.UserRepo;
 import com.smcomanager.SCM_Entity.Providers;
@@ -57,12 +55,10 @@ public class OAuthServices implements AuthenticationSuccessHandler {
         user.setEmailVerified(true);
         user.setEnable(true);
         user.setPassword("dummy");
-
         if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
 
             // google
             // google attributes
-
             user.setEmail(oauthUser.getAttribute("email").toString());
             user.setProfilePic(oauthUser.getAttribute("picture").toString());
             user.setName(oauthUser.getAttribute("name").toString());
@@ -90,14 +86,11 @@ public class OAuthServices implements AuthenticationSuccessHandler {
         }
 
         else if (authorizedClientRegistrationId.equalsIgnoreCase("linkedin")) {
-
         }
 
         else {
             logger.info("OAuthAuthenicationSuccessHandler: Unknown provider");
         }
-
-    
 
         Users user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
         if (user2 == null) {
